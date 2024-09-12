998a847f3d8808fd3babd83b658970fddb75b706
3CI_BURLA_THOMAS_2023_24 <20164@studenti.marconiverona.edu.it>
2024-09-12 19:43:36 +0200

    Update Negozio.java

--- Settembre/Negozio_12_09_Burla/Negozio.java
+++ Settembre/Negozio_12_09_Burla/Negozio.java
@@ -13,4 +13,26 @@ public class Negozio {
     private String nome;
     private String city;
     private ArrayList <Cliente> clienti;
+    //costruttore senza parametri
+    public Negozio(){
+        this.nome = "-";
+        this.city = "-";
+        this.clienti = new ArrayList <Cliente> ();
+    }
+    //costruttore con parametri
+    public Negozio(String nome, String city, Cliente cliente){
+        //nome
+        if(nome != null && (!(nome.equals(" ")))){
+            this.nome = nome;
+        }
+        //city
+        if(city != null && (!(city.equals(" ")))){
+            this.city = city;
+        }
+        //clienti
+        this.clienti = new ArrayList <Cliente> ();
+        if(cliente != null){
+            this.clienti.add(cliente);
+        }
+    }
 }
