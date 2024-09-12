bbaaeaa462fb24b8cece5322d73362c3f2db1f7b
3CI_BURLA_THOMAS_2023_24 <20164@studenti.marconiverona.edu.it>
2024-09-12 18:41:35 +0200

    inizio classe Negozio

---
+++ Settembre/Negozio_12_09_Burla/Negozio.java
@@ -0,0 +1,16 @@
+/**
+ * classe Negozio:
+    -nome
+    -city
+    -Clienti[]
+ * 
+ * @author Thomas Burla
+ * @version 1.0
+ */
+import java.util.*;
+public class Negozio {
+    //variabili d'istanza
+    private String nome;
+    private String city;
+    private ArrayList <Cliente> clienti;
+}
