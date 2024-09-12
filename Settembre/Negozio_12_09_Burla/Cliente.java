c1adfe66b9193292993ad505d22ba18cd7e16eba
3CI_BURLA_THOMAS_2023_24 <20164@studenti.marconiverona.edu.it>
2024-09-12 18:32:19 +0200

    creata classe Cliente

---
+++ Settembre/Negozio_12_09_Burla/Cliente.java
@@ -0,0 +1,100 @@
+/**
+ * classe Cliente:
+    -nome
+    -cognome
+    -email
+    -num_cell
+    -Carrelli[]
+ * 
+ * @author Thomas Burla
+ * @version 1.0
+ */
+import java.util.*;
+public class Cliente {
+    //variabili d'istanza
+    private String nome;
+    private String cognome;
+    private String email;
+    private long num_cell;
+    private ArrayList <Carrello> carrelli;
+    //costruttore senza parametri
+    public Cliente(){
+        this.nome = "-";
+        this.cognome = "-";
+        this.email = "-";
+        this.num_cell = 0000000000;
+        this.carrelli = new ArrayList <Carrello> ();
+    }
+    //costruttore con parametri controllati
+    public Cliente(String nome, String cognome, String email, long num_cell, Carrello carrello){
+        //nome
+        if(nome != null && (!(nome.equals(" ")))){
+            this.nome = nome;
+        }else{
+            this.nome = "-";
+        }
+        //cognome
+        if(cognome != null && (!(cognome.equals(" ")))){
+            this.cognome = cognome;
+        }else{
+            this.cognome = "-";
+        }
+        //email
+        if(email != null && (!(email.equals(" ")))){
+            this.email = email;
+        }else{
+            this.email = "-";
+        }
+        //num_cell
+        if(num_cell > 0){
+            this.num_cell = num_cell;
+        }else{
+            this.num_cell = 0;
+        }
+        //Carrelli[]
+        this.carrelli = new ArrayList <Carrello> ();
+        if(carrello != null){
+            this.carrelli.add(carrello);
+        }
+    }
+    //set nome
+    public void setNome(String nome){
+        if(nome != null && (!(nome.equals(" ")))){
+            this.nome = nome;
+        }
+    }
+    //get nome
+    public String getNome(){
+        return this.nome;
+    }
+    //set cognome
+    public void setCognome(String cognome){
+        if((cognome != null) && (!(cognome.equals(" ")))){
+            this.cognome = cognome;
+        }
+    }
+    //get cognome
+    public String getCognome(){
+        return this.cognome;
+    }
+    //set email
+    public void setEmail(String email){
+        if((email != null) && (!(email.equals(" ")))){
+            this.email = email;
+        }
+    }
+    //get cognome
+    public String getEmail(){
+        return this.email;
+    }
+    //set num_cell
+    public void setNum_cell(long num_cell){
+        if(num_cell > 0){
+            this.num_cell = num_cell;
+        }
+    }
+    //get num_cell
+    public long getNum_cell(){
+        return this.num_cell;
+    }
+}
