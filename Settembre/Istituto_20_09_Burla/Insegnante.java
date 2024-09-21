acaf5cbdd6295032e1cceb56843ecf56583e7a0c
3CI_BURLA_THOMAS_2023_24 <20164@studenti.marconiverona.edu.it>
2024-09-21 17:55:06 +0200

    Update Insegnante.java
    classe "in sospeso"...fare prima classe Classe e relativi metodi che serviranno per terminare la classe Insegnante

--- Settembre/Istituto_20_09_Burla/Insegnante.java
+++ Settembre/Istituto_20_09_Burla/Insegnante.java
@@ -37,4 +37,105 @@ public class Insegnante extends Persona{
             this.classi.add(classe);
         }
     }
+    //set nome
+    public void setNome(String nome){
+        if((nome != null) && (!(nome.equalsIgnoreCase(" ")))){
+            this.nome = nome;
+        }
+    }
+    //get nome
+    public String getNome(){
+        return this.nome;
+    }
+    //set cognome
+    public void setCognome(String cognome){
+        if((cognome != null) && (!(cognome.equalsIgnoreCase(" ")))){
+            this.cognome = cognome;
+        }
+    }
+    //get cognome
+    public String getCognome(){
+        return this.cognome;
+    }
+    //set email
+    public void setEmail(String email){
+        if((email != null) && (!(email.equalsIgnoreCase(" ")))){
+            this.email = email;
+        }
+    }
+    //get email
+    public String getEmail(){
+        return this.email;
+    }
+    //set codice fiscale
+    public void setcodFiscale(String codFiscale){
+        if((codFiscale != null) && (!(codFiscale.equalsIgnoreCase(" ")))){
+            this.codFiscale = codFiscale;
+        }    
+    }
+    //get codice fiscale
+    public String getcodFiscale(){
+        return this.codFiscale;
+    }
+    //set numero di cellullare
+    public void setNumCell(String numCell){
+        if((numCell != null) && (!(numCell.equalsIgnoreCase(" ")))){
+            this.numCell = numCell;
+        }
+    }
+    //get numero di cellulare
+    public String getNumCell(){
+        return this.numCell;
+    }
+    //set corso
+    public void setCorso(String corso){
+        if((corso != null) && (!(corso.equalsIgnoreCase(" ")))){
+            this.corso = corso;
+        }
+    }
+    //get corso
+    public String getCorso(){
+        return this.corso;
+    }
+    //set stipendio
+    public void setStipendio(double stipendio){
+        if(stipendio > 0.0){
+            this.stipendio = stipendio;
+        }
+    }
+    public double getStipendio(){
+        return this.stipendio;
+    }
+    //assegna nuova classe
+    public void addClasse(Classe classe){
+        if(classe != null){
+            this.classi.add(classe);
+        }
+    }
+
+    
+    /*
+        //cerca classe tra quelle assegnate e ottieni info toString
+        public String getClasseStringa(String nome){
+
+        }
+        public Classe getClasse(String nome){
+
+        }
+        public boolean rimuoviClasse(String nome){
+            
+        }
+    */
+
+    //toString
+    public String toString(){
+        String out = "La persona si chiama " + this.nome + " " +  this.cognome;
+        out += ", la sua email è: |" + this.email;
+        out += "|, il suo numero di cellulare è: |" + this.numCell;
+        out += "|, il suo codice fiscale è: |" + this.codFiscale;
+        out += "|, insegna la disciplina " + this.corso;
+        out += ", il suo stipendio all'ora è di " + this.stipendio + "euro";
+        out += "e insegna nelle seguenti classi:";
+        return out;
+    }   
 }
