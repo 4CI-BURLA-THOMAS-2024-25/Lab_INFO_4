e56fcf788aae9d2d547da7b8a9c4b6f020fee318
3CI_BURLA_THOMAS_2023_24 <20164@studenti.marconiverona.edu.it>
2024-09-21 16:51:38 +0200

    creati costruttori

--- Settembre/Istituto_20_09_Burla/Studente.java
+++ Settembre/Istituto_20_09_Burla/Studente.java
@@ -1,3 +1,23 @@
-public class Studente {
-    
+/**
+ * classe Studente, che eredita variabili dalla classe Persona:
+    -variabili di classe Persona
+    -matricola
+ *
+ * @author Thomas Burla
+ * @version 1.0
+ */
+public class Studente extends Persona{
+    private int matricola;
+    //costruttore senza parametri
+    public Studente(){
+        super();
+        this.matricola = 0;
+    }
+    //costruttore con parametri
+    public Studente(String nome, String cognome, String email, String codFiscale, String numCell, int matricola){
+        super(nome, cognome, email, codFiscale, numCell);
+        if(matricola > 0){
+            this.matricola = matricola;
+        }
+    }
 }
