c16cfa14ad807cbcbe7789ef9ebbd30c84acecc5
3CI_BURLA_THOMAS_2023_24 <20164@studenti.marconiverona.edu.it>
2024-09-23 17:06:13 +0200

    aggiunti costruttori

--- Settembre/Istituto_20_09_Burla/Scuola.java
+++ Settembre/Istituto_20_09_Burla/Scuola.java
@@ -1,3 +1,38 @@
+/**
+ * classe Scuola:
+    -Classi[]
+    -codice meccanografico
+ * 
+ * @author Thomas Burla
+ * @version 1.0
+ */
+import java.util.ArrayList;
 public class Scuola {
-    
+    private ArrayList <Classe> classi;
+    private ArrayList <PersonaleATA> personaleATA;
+    private String codice;
+    //costruttore senza parametri
+    public Scuola(){
+        this.classi = new ArrayList <Classe> ();
+        this.personaleATA = new ArrayList <PersonaleATA> ();
+        this.codice = "-";
+    }
+    //costruttore con parametri
+    public Scuola(Classe classe, PersonaleATA personaleATA, String codice){
+        //creo liste
+        this.classi = new ArrayList <Classe> ();
+        this.personaleATA = new ArrayList <PersonaleATA> ();
+        //classi
+        if(classe != null){
+            this.classi.add(classe);
+        }
+        //personale ata
+        if(personaleATA != null){
+            this.personaleATA.add(personaleATA);
+        }
+        //codice meccanografico
+        if((codice != null) && (!(codice.equalsIgnoreCase(" ")))){
+            this.codice = codice;
+        }
+    }
 }
