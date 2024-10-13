341132f2071d2485eef46f77f1709e91ff78ebd0
3CI_BURLA_THOMAS_2023_24 <20164@studenti.marconiverona.edu.it>
2024-10-13 11:19:00 +0200

    set, get, volume, superficie e toString

--- Ottobre/Lab_07_10_ClassiAstratte_Burla/src/main/java/Cubo.java
+++ Ottobre/Lab_07_10_ClassiAstratte_Burla/src/main/java/Cubo.java
@@ -1,13 +1,63 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
- *
- * @author 20164
+ * classe Cubo, che eredita la classe astratta Solido
+ * 
+ * @author Thomas Burla
+ * @version 1.0
  */
-public class Cubo {
-
+public class Cubo extends Solido{
+    //variabili d'istanza
+    private double lato;
+    //costruttore senza parametri
+    public Cubo(){
+        super();
+        this.lato = 0.0;
+    }
+    //costruttore con parametri
+    public Cubo(double pesoSpecifico, double lato){
+        //invoco coatruttore classe padre
+        super(pesoSpecifico);
+        //lato
+        if(lato > 0.0){
+            this.lato = lato;
+        }
+    }
+    //set lato
+    public boolean setLato(double lato){
+        if(lato > 0.0){
+            this.lato = lato;
+            return true;
+        }
+        return false;
+    }
+    //get lato
+    public double getLato(){
+        return this.lato;
+    }
+    //volume
+    @Override
+    public double volume(){
+        if(this.lato > 0.0){
+            return Math.pow(this.lato, 3);
+        }
+        return -1;
+    }
+    //superificie
+    @Override
+    public double superficie(){
+        if(this.lato > 0.0){
+            return 6 * (Math.pow(this.lato, 2));
+        }
+        return -1;
+    }
+    //toString
+    @Override
+    public String toString(){
+        if(this.lato > 0.0){
+            String out = "Il lato del cubo misura: " + this.lato;
+            out += "m;\nla superificie del solido è: " + 6 * (Math.pow(this.lato, 2));
+            out += "m^2\nil volume del solido è: " + Math.pow(this.lato, 3);
+            return out;
+        }
+        return "Dati NON validi";
+    }
 }
