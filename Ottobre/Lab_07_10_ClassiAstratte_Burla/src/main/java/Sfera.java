af9df03246baa4672a12e2f4e7ff2b3629967819
3CI_BURLA_THOMAS_2023_24 <20164@studenti.marconiverona.edu.it>
2024-10-13 11:32:36 +0200

    set, get, volume, superficie e toString

--- Ottobre/Lab_07_10_ClassiAstratte_Burla/src/main/java/Sfera.java
+++ Ottobre/Lab_07_10_ClassiAstratte_Burla/src/main/java/Sfera.java
@@ -1,13 +1,65 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
- *
- * @author 20164
+ * classe Sfera, che eredita la classe astratta Solido
+ * 
+ * @author Thomas Burla
+ * @version 1.0
  */
-public class Sfera {
-
+public class Sfera extends Solido{
+    //variabili d'istanza
+    private double raggio;
+    //costruttore senza parametri
+    public Sfera(){
+        super();
+        this.raggio = 0.0;
+    }
+    //costruttore con parametri
+    public Sfera(double pesoSpecifico, double raggio){
+        //invoco costruttore della classe padre
+        super(pesoSpecifico);
+        //raggio
+        if(raggio > 0.0){
+            this.raggio = raggio;
+        }
+    }
+    //set raggio
+    public boolean setRaggio(double raggio){
+        if(raggio > 0.0){
+            this.raggio = raggio;
+            return true;
+        }
+        return false;
+    }
+    //get raggio
+    public double getRaggio(){
+        return this.raggio;
+    }
+    //volume
+    @Override
+    public double volume(){
+        if(this.raggio > 0.0){
+            return (4 * Math.PI * Math.pow(this.raggio, 3)) / 3.0;
+        }
+        return -1;
+    }
+    //superficie
+    @Override
+    public double superficie(){
+        if(this.raggio > 0.0){
+            return 4 * Math.PI * Math.pow(this.raggio, 2);
+        }
+        return -1;
+    }
+    //toString
+    @Override
+    public String toString(){
+        if(this.raggio > 0.0){
+            double volume = (4 * Math.PI * Math.pow(this.raggio, 3)) / 3.0;
+            double superficie = 4 * Math.PI * Math.pow(this.raggio, 2);
+            String out = "Il raggio della sfera è: " + this.raggio;
+            out += "m;\nla superficie del solido misura: " + superficie;
+            out += "m^2;\nil volume del solido è: " + volume + "m^3";
+            return out;
+        }
+        return "Dati NON validi";
+    }
 }
