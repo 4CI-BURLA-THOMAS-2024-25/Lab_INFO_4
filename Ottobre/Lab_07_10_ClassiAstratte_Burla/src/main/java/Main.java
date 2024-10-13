ddf497cd0e97ff39b0a1c56f66185619ff9942c1
3CI_BURLA_THOMAS_2023_24 <20164@studenti.marconiverona.edu.it>
2024-10-13 12:55:44 +0200

    caso cono + correzione pesoSpecifico

--- Ottobre/Lab_07_10_ClassiAstratte_Burla/src/main/java/Main.java
+++ Ottobre/Lab_07_10_ClassiAstratte_Burla/src/main/java/Main.java
@@ -7,6 +7,8 @@
 import javax.swing.JOptionPane;
 import java.util.ArrayList;
 public class Main {
+    //indico al compilatore di non controllare i tipi di dati che vengono aggiunti alle arraylist (in quanto la lista dei solidi deve contenere diversi tipi di dati)
+    @SuppressWarnings("unchecked")
     public static void main(String args[]){
         //variabile utilizzata per far scegliere all'utente cosa fare dal menu
         int opzione;
@@ -27,28 +29,32 @@ public class Main {
         //ciclo per aggiungere solidi finchè l'utente lo desidera
         do{
             //chiedo all'utente cosa fuore fare dal menu
-            opzione = Integer.parseInt(JOptionPane.showInputDialog(null, "BENVENUTO! Questo programma ti permette di effettuare dei calcoli su dei solidi geometrici. Scegli con quale solido vuoi operare, digitando un numero del seguente menu:\n1.cilindro;\n2.cono;\n3.Cubo;\n4.Sfera." , "Scegli solido", JOptionPane.PLAIN_MESSAGE));
+            opzione = Integer.parseInt(JOptionPane.showInputDialog(null, "BENVENUTO! Questo programma ti permette di effettuare dei calcoli su dei solidi geometrici. Scegli con quale solido vuoi operare, digitando un numero del seguente menu:\n1.cilindro;\n2.cono;\n3.cubo;\n4.sfera." , "Scegli solido", JOptionPane.PLAIN_MESSAGE));
+            //se l'utente sceglie una delle opzioni proposte, chiedo peso specifico del solido
+            if((opzione == 1) || (opzione == 2) || (opzione == 3) || (opzione == 4)){
+                //leggo e controllo peso specifico, che viene sempre richiesto indipendentemente dal solido
+                do{
+                    //variabile impostata a true nel caso in cui si inserisca un tipo di dato non atteso, di base impostata a false
+                    incompatibile = false;
+                    //verifico che l'utente inserisca un double
+                    try{
+                        //leggo pesoSpecifico
+                        pesoSpecifico = Double.parseDouble(JOptionPane.showInputDialog(null, "Inserire il peso specifico del solido", "Inserisci peso specifico", JOptionPane.QUESTION_MESSAGE));
+                        //messaggio di errore in caso di valore non valido
+                        if(pesoSpecifico <= 0.0){
+                            JOptionPane.showMessageDialog(null, "ERRORE! Valore non valido" , "Errore" , JOptionPane.ERROR_MESSAGE);
+                        }
+                    //caso in cui l'utente non digiti un double
+                    }catch(NumberFormatException e){
+                        incompatibile = true;
+                        JOptionPane.showMessageDialog(null, "ERRORE! Tipo di dato incompatibile", "Dato non compatibile", JOptionPane.ERROR_MESSAGE);
+                    }
+                }while((pesoSpecifico <= 0.0) || (incompatibile == true));
+            }
             //esecuzione diversificata in base all'opzione scelta
             switch(opzione){
+                //caso cilindro
                 case 1:{
-                    //leggo e controllo peso specifico
-                    do{
-                        //variabile impostata a true nel caso in cui si inserisca un tipo di dato non atteso, di base impostata a false
-                        incompatibile = false;
-                        //verifico che l'utente inserisca un double
-                        try{
-                            //leggo pesoSpecifico
-                            pesoSpecifico = Double.parseDouble(JOptionPane.showInputDialog(null, "Inserire il peso specifico del solido", "Inserisci peso specifico", JOptionPane.QUESTION_MESSAGE));
-                            //messaggio di errore in caso di valore non valido
-                            if(pesoSpecifico <= 0.0){
-                                JOptionPane.showMessageDialog(null, "ERRORE! Valore non valido" , "Errore" , JOptionPane.ERROR_MESSAGE);
-                            }
-                        //caso in cui l'utente non digiti un double
-                        }catch(NumberFormatException e){
-                            incompatibile = true;
-                            JOptionPane.showMessageDialog(null, "ERRORE! Tipo di dato incompatibile", "Dato non compatibile", JOptionPane.ERROR_MESSAGE);
-                        }
-                    }while((pesoSpecifico <= 0.0) || (incompatibile = true));
                     //leggo e controllo raggio
                     do{
                         //variabile impostata a true nel caso in cui si inserisca un tipo di dato non atteso, di base impostata a false
@@ -66,7 +72,7 @@ public class Main {
                             incompatibile = true; 
                             JOptionPane.showMessageDialog(null, "ERRORE! Tipo di dato incompatibile", "Dato non compatibile", JOptionPane.ERROR_MESSAGE);
                         }
-                    }while((raggio <= 0.0) || (incompatibile = true));
+                    }while((raggio <= 0.0) || (incompatibile == true));
                     //leggo e controllo altezza
                     do{
                         //variabile impostata a true nel caso in cui si inserisca un tipo di dato non atteso, di base impostata a false
@@ -84,20 +90,70 @@ public class Main {
                             incompatibile = true; 
                             JOptionPane.showMessageDialog(null, "ERRORE! Tipo di dato incompatibile", "Dato non compatibile", JOptionPane.ERROR_MESSAGE);
                         }
-                    }while((altezza <= 0.0) || (incompatibile = true));
+                    }while((altezza <= 0.0) || (incompatibile == true));
                     //creo oggetto Cilindro
                     Cilindro cilindro = new Cilindro(raggio, altezza, pesoSpecifico);
+                    //stampo info e calcoli
+                    System.out.println(cilindro.toString());
+                    //salvo cilindro nella lista dei solidi
                     solidi.add(cilindro);
                     break;
                 }
+                //caso cono
                 case 2:{
-
+                    //leggo e controllo raggio
+                    do{
+                        //variabile impostata a true nel caso in cui si inserisca un tipo di dato non atteso, di base impostata a false
+                        incompatibile = false;
+                        //verifico che l'utente inserisca un double
+                        try{
+                            //leggo raggio
+                            raggio = Double.parseDouble(JOptionPane.showInputDialog(null, "Inserire il raggio della base del solido", "Inserisci raggio", JOptionPane.QUESTION_MESSAGE));
+                            //messaggio di errore in caso di valore non valido
+                            if(raggio <= 0.0){
+                                JOptionPane.showMessageDialog(null, "ERRORE! Valore non valido" , "Errore" , JOptionPane.ERROR_MESSAGE);
+                            }
+                        //caso in cui l'utente non digiti un double
+                        }catch(NumberFormatException e){
+                            incompatibile = true; 
+                            JOptionPane.showMessageDialog(null, "ERRORE! Tipo di dato incompatibile", "Dato non compatibile", JOptionPane.ERROR_MESSAGE);
+                        }
+                    }while((raggio <= 0.0) || (incompatibile == true));
+                    //leggo e controllo altezza
+                    do{
+                        //variabile impostata a true nel caso in cui si inserisca un tipo di dato non atteso, di base impostata a false
+                        incompatibile = false;
+                        //verifico che l'utente inserisca un double
+                        try{
+                            //leggo altezza
+                            altezza = Double.parseDouble(JOptionPane.showInputDialog(null, "Inserire l'altezza del solido", "Inserisci altezza", JOptionPane.QUESTION_MESSAGE));
+                            //messaggio di errore in caso di valore non valido
+                            if(altezza <= 0.0){
+                                JOptionPane.showMessageDialog(null, "ERRORE! Valore non valido" , "Errore" , JOptionPane.ERROR_MESSAGE);
+                            }
+                        //caso in cui l'utente non digiti un double
+                        }catch(NumberFormatException e){
+                            incompatibile = true; 
+                            JOptionPane.showMessageDialog(null, "ERRORE! Tipo di dato incompatibile", "Dato non compatibile", JOptionPane.ERROR_MESSAGE);
+                        }
+                    }while((altezza <= 0.0) || (incompatibile == true));
+                    //creo oggetto Cono
+                    Cono cono = new Cono(pesoSpecifico, raggio, altezza);
+                    //stampo info e calcoli
+                    System.out.println(cono.toString());
+                    //salvo cono nella lista dei solidi
+                    solidi.add(cono);
+                    break;
+                }
+                //caso cubo
+                case 3:{
+                    
                 }
                 default:{
                     JOptionPane.showMessageDialog(null, "ERRORE! Opzione inesistente", "Errore", JOptionPane.ERROR_MESSAGE);
                 }
             }
             menu = JOptionPane.showConfirmDialog(null, "Tornare al menu?", "Torna al menu", JOptionPane.YES_NO_OPTION);
-        }while(menu == JOptionPane.YES_OPTION);
+        }while((menu == JOptionPane.YES_OPTION));
     }
 }
