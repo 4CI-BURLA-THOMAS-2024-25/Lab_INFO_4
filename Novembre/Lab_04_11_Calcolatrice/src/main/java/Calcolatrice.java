ead9ed934e7ecc6e6dbec598e4bc3d8400506329
3CI_BURLA_THOMAS_2023_24 <20164@studenti.marconiverona.edu.it>
2024-11-17 10:05:17 +0100

    creo ascoltatori numeri e operatori

--- Novembre/Lab_04_11_Calcolatrice/src/main/java/Calcolatrice.java
+++ Novembre/Lab_04_11_Calcolatrice/src/main/java/Calcolatrice.java
@@ -5,7 +5,9 @@
  * @version 1.0
  */
 import javax.swing.*;
+import java.awt.event.*;
 import java.awt.*;
+import java.util.ArrayList;
 public class Calcolatrice extends JFrame{
     //variabili d'istanza
     private Container contenitore;
@@ -14,10 +16,18 @@ public class Calcolatrice extends JFrame{
     private JPanel pannelloMain;
     private JPanel pannelloNumeri;
     private JPanel pannelloOperatori;
+    private String display = "";
+    private ArrayList <JButton> bottoni;
+    private double numero1;
+    private double numero2;
+    private String operatore;
+    private double risultato;
     //costruttore, con nome della finestra
     public Calcolatrice(String nome){
         //invoco costruttore della finestra
         super(nome);
+        //alloco arraylist
+        bottoni = new ArrayList <JButton> ();
         //assegno al contenitore il contenitore della finestra JFrame
         contenitore = this.getContentPane();
         //creo pannello principale con BorderLayout
@@ -30,37 +40,26 @@ public class Calcolatrice extends JFrame{
         for(int i = 0; i <= 9; i++){
             //creo bottone con il numero
             bottone = new JButton(Integer.toString(i));
+            //associo nuovo ascoltatore al bottone
+            bottone.addActionListener(new AscoltatoreNumeri());
             //aggiungo bottone al pannello
-            pannelloNumeri.add(bottone, BorderLayout.CENTER);
+            pannelloNumeri.add(bottone);
+            //aaggiungo bottone alla lista dei bottoni
+            bottoni.add(bottone);
         }
-        //creo bottone per la somma e lo aggiungo al pannello
-        bottone = new JButton("+");
-        //aggiugno bottone al pannello
-        pannelloOperatori.add(bottone, BorderLayout.CENTER);
-        //creo bottone per la sottrazione e lo aggiungo al pannello
-        bottone = new JButton("-");
-        //aggiugno bottone al pannello
-        pannelloOperatori.add(bottone, BorderLayout.CENTER);
-        //creo bottone per la moltiplicazione e lo aggiungo al pannello
-        bottone = new JButton("*");
-        //aggiugno bottone al pannello
-        pannelloOperatori.add(bottone, BorderLayout.CENTER);
-        //creo bottone per la divisione e lo aggiungo al pannello
-        bottone = new JButton("/");
-        //aggiugno bottone al pannello
-        pannelloOperatori.add(bottone, BorderLayout.CENTER);
-        //creo bottone per la cancellare e lo aggiungo al pannello
-        bottone = new JButton("CANC");
-        //aggiugno bottone al pannello
-        pannelloOperatori.add(bottone, BorderLayout.CENTER);
-        //creo bottone punto e lo aggiungo al pannello
-        bottone = new JButton(".");
-        //aggiugno bottone al pannello
-        pannelloOperatori.add(bottone, BorderLayout.CENTER);
-        //creo bottone uguale e lo aggiungo al pannello
-        bottone = new JButton("=");
-        //aggiugno bottone al pannello
-        pannelloOperatori.add(bottone, BorderLayout.CENTER);
+        //creo array di stringhe per gli operatori
+        String operatori[] = {"+", "-", "*", "/", "CANC", ".", "="};
+        //creo bottoni con gli operatori e li aggiungo al loro pannello
+        for(int i = 0; i < operatori.length; i++){
+            //creo bottone
+            bottone = new JButton(operatori[i]);
+            //associo nuovo ascoltatore al bottone
+            bottone.addActionListener(new AscoltatoreOperatori());
+            //aggiungo bottone al pannello degli operatori
+            pannelloOperatori.add(bottone);
+            //aggiungo bottone alla lista dei bottoni
+            bottoni.add(bottone);
+        }
         //creo casella di testo per mostrare quello digitato
         testo = new JTextField(50);
         //rendo la casella di testo non modificabile dall'utente essendo che deve servire solo a mostrare i pulsanti cliccati
@@ -79,5 +78,58 @@ public class Calcolatrice extends JFrame{
         this.setVisible(true);
         //imposto chiusura finestra
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    
+    public class AscoltatoreNumeri implements ActionListener{
+        //implemento metodo
+        public void actionPerformed(ActionEvent e){
+            //ottengo testo del bottone così da sapere quale numero è stato cliccato
+            String comando = e.getActionCommand();
+            //aggiorno testo associato al display
+            display += comando;
+            //aggiorno testo visualizzato sul display
+            testo.setText(display);
+        }
+    }
+    public class AscoltatoreOperatori implements ActionListener{
+        //implemento metodo
+        public void actionPerformed(ActionEvent e){
+            //ottengo testo del bottone così da sapere quale numero è stato cliccato
+            String comando = e.getActionCommand();
+            if((comando.equals("+")) || (comando.equals("-")) || (comando.equals("*")) || (comando.equals("/"))){
+                //assegno al primo numero il valolre visualizzato sul display
+                numero1 = Double.parseDouble(display);
+                operatore = comando;
+                //aggiorno testo associato al display
+                display += comando;
+                //aggiorno testo visualizzato sul display
+                testo.setText(display);
+            }
+            else if((comando.equals("=")) && (numero2 != 0)){
+                switch(operatore){
+                    case "+":{
+                        risultato = numero1 + numero2;
+                        break;
+                    }
+                    case "-":{
+                        risultato = numero1 - numero2;
+                        break;
+                    }
+                    case "*":{
+                        risultato = numero1 * numero2;
+                        break;
+                    }
+                    case "/":{
+                        risultato = numero1 / numero2;
+                        break;
+                    }
+                }
+                //aggiorno testo associato al display
+                display = Double.toString(risultato);
+                //aggiorno testo visualizzato sul display
+                testo.setText(display);
+                
+            }
+        }
     }
 }
