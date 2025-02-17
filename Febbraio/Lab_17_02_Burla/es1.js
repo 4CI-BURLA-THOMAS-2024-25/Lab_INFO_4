eb1eb7b21df6ad7c48f6db9bec00b9c1a8857263
4CI_BURLA_THOMAS_2024_25 <20164@studenti.marconiverona.edu.it>
2025-02-17 14:11:56 +0100

    inizio es1

---
+++ Febbraio/Lab_17_02_Burla/es1.js
@@ -0,0 +1,7 @@
+function visualizzaFinestrella(){
+    const campiForm = document.forms["form"];
+    const nome = campiForm[0];
+    const cognome = campiForm[1];
+    const email = campiForm[2];
+    window.alert("Benvenuto "$);
+}
